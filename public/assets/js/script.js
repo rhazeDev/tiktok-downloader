99e02a995dc08834f90fee3279d7e246126a462a
Justine Agcanas <agcanasjustine1@gmail.com>
2024-03-05 19:54:54 +0800

    Initial commit

---
+++ public/assets/js/script.js
@@ -0,0 +1,56 @@
+const pasteBtn = document.getElementById('paste');
+const downloadBtn = document.getElementById('download');
+const url = document.getElementById('url');
+const cover = document.getElementById('cover');
+const title = document.getElementById('title');
+const creator = document.getElementById('creator');
+const duration = document.getElementById('duration');
+const size = document.getElementById('size');
+const result_container = document.getElementsByClassName('result-container');
+
+pasteBtn.addEventListener('click', async () => {
+    const text = await navigator.clipboard.readText();
+    url.value = text;
+});
+
+downloadBtn.addEventListener('click', async () => {
+    const response = await fetch('/api/download', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url: url.value })
+    });
+    const data = await response.json();
+    
+    if (data.status == "success") {
+        cover.src = data.data.cover;
+        title.textContent = data.data.title;
+        creator.textContent = "- " + data.data.creator;
+        duration.textContent = data.data.duration;
+        size.textContent = data.data.size;
+
+        result_container[0].style.display = "block";
+
+        // download video
+        downloadVideo(data.data.download_link, data.data.title)
+       
+
+    } else {
+        alert(data.message);
+    }
+});
+
+async function downloadVideo(videoUrl, fileName) {
+    alert("Downloaded started, please wait...")
+    const response = await fetch(videoUrl);
+    const blob = await response.blob();
+
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = fileName + '.mp4';
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
---
+++ server.js
@@ -0,0 +1,73 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import request from "request-promise";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/api/download', async (req, res) => {
+    const tiktokUrl = req.body.url;
+
+    if (!tiktokUrl) {
+        return res.json({ status: "error", message: "URL is required" });
+    }
+
+    const options = {
+        url: 'https://www.tikwm.com/api/',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            url: tiktokUrl,
+            count: 12,
+            cursor: 0,
+            web: 1,
+            hd: 1
+        })
+    };
+
+    try {
+        const response = await request(options);
+        const data = JSON.parse(response);
+        const msg = data.msg;
+
+        if (msg == "success") {
+            const duration = data.data.duration / 60;
+            const size = data.data.size / 1000000;
+
+
+            res.json({ status: "success",
+                data: {
+                    download_link: `https://www.tikwm.com${data.data.hdplay}`,
+                    cover: `https://www.tikwm.com${data.data.cover}`,
+                    title: data.data.title,
+                    creator: data.data.author.nickname,
+                    duration: duration.toFixed(2) + " minutes",
+                    size: size.toFixed(2) + "MB"
+                }
+            });
+        } else {
+            res.json({ status: "error", message: data.msg });
+        }
+    } catch (error) {
+        res.json({ status: "error", message: error.message });
+    }
+
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Listening on port:", port);
+});
